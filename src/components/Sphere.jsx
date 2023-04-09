7e9e5d7dd0e55ae72e24bc56d81ad9fddcf9ab53
Aarón Maldonado <sekamdex@gmail.com>
2023-04-09 14:30:51 -0600

    initial commit

---
+++ src/components/Sphere.jsx
@@ -0,0 +1,66 @@
+import { useRef, useEffect, useState } from "react";
+import gsap from "gsap";
+
+const getRandomArbitrary = (min, max) => Math.random() * (max - min) + min;
+
+const animationTime = 0.15;
+
+gsap.registerEffect({
+  name: "sphereScaleAnimation",
+  effect: (targets, config) => {
+    return gsap.fromTo(
+      targets,
+      {
+        x: config.sphereProps.scale,
+        y: config.sphereProps.scale,
+        z: config.sphereProps.scale,
+      },
+      {
+        ease: "power2.inOut",
+        duration: 0.25,
+        yoyo: true,
+        repeat: 1,
+        x: config.scaleFactor * config.sphereProps.scale,
+        y: config.scaleFactor * config.sphereProps.scale,
+        z: config.scaleFactor * config.sphereProps.scale,
+      }
+    );
+  },
+  defaults: { duration: animationTime },
+  extendTimeline: true,
+});
+
+export const Sphere = ({ color }) => {
+  const [sphereTimeline, setSphereTimeline] = useState(null);
+
+  const [sphereProps] = useState({
+    position: [
+      getRandomArbitrary(-7, 7),
+      getRandomArbitrary(-7, 7),
+      getRandomArbitrary(-7, 7),
+    ],
+    scale: getRandomArbitrary(0.5, 2),
+  });
+
+  const mesh = useRef();
+
+  useEffect(() => {
+    setSphereTimeline(() => {
+      const tl = gsap.timeline({ paused: true });
+      const scaleFactor = getRandomArbitrary(1.25, 3);
+
+      tl.sphereScaleAnimation(mesh.current?.scale, {
+        sphereProps,
+        scaleFactor,
+      });
+      return tl;
+    });
+  }, [sphereProps]);
+
+  return (
+    <mesh ref={mesh} position={sphereProps.position} scale={sphereProps.scale}>
+      <sphereGeometry />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+};
---
+++ src/components/Spheres.jsx
@@ -0,0 +1,38 @@
+import {
+  OrbitControls,
+  Environment,
+  OrthographicCamera,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useState } from "react";
+import chroma from "chroma-js";
+
+import { Sphere } from "./Sphere";
+
+export default function Spheres() {
+  const count = 46;
+  const [colors] = useState(() => chroma.scale("Spectral").colors(count));
+
+  const renderSpheres = (count, content = []) => {
+    for (let i = 0; i < count; i++) {
+      content.push(<Sphere color={colors[i]} key={i} />);
+    }
+    return content;
+  };
+
+  return (
+    <>
+      <Canvas className="h-24 w-full rounded-lg">
+        <color attach="background" args={["#075985"]} />
+        <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={50} />
+        <ambientLight intensity={0.05} />
+        <Suspense fallback={null}>
+          {renderSpheres(count)}
+          <Environment preset="sunset" />
+        </Suspense>
+        <OrbitControls enablePan={false} enableZoom={false} autoRotate={true} />
+      </Canvas>
+    </>
+  );
+}
